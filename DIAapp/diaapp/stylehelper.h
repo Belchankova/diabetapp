8fea49cda9171608ac91cb6e4195a4d41755b70a
Екатерина Бельчанкова <ekaterinabel29@gmail.com>
2024-05-17 21:13:10 +0300

    First commit

---
+++ DIAapp/diaapp/stylehelper.h
@@ -0,0 +1,11 @@
+#ifndef STYLEHELPER_H
+#define STYLEHELPER_H
+#include <QString>
+class stylehelper
+{
+public:
+    static QString getMainWindowStyle();
+    static QString getStartTabWidgetStyle();
+};
+
+#endif // STYLEHELPER_H
